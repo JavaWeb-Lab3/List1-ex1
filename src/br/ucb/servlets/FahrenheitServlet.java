6de548ab62a37d687666ded7c2fa5a93dfe29a74
fmarques55 <fmarques899@gmail.com>
2017-03-01 23:50:53 -0300

    Exercicio 1

---
+++ src/br/ucb/servlets/FahrenheitServlet.java
@@ -0,0 +1,44 @@
+package br.ucb.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FahrenheitServlet
+ */
+@WebServlet("/FahrenheitServlet")
+public class FahrenheitServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FahrenheitServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String celsiusTempString = request.getParameter("temperature_in_celsius");
+		PrintWriter out = response.getWriter();
+		out.println(celsiusTempString);
+	}
+
+}
